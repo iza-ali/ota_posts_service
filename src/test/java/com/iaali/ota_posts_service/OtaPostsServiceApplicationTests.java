5873cb1c8a73f1f786d88404306e8537a9aadeb6
Ali <izabella.ali@accenture.com>
2025-07-31 17:36:10 +0300

    initial project commit

---
+++ src/test/java/com/iaali/ota_posts_service/OtaPostsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iaali.ota_posts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaPostsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
